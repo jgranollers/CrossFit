a18f2335fa9b3357b8cc9d110a14d16eeafc8c5e
jgranollers <juditgra10@gmail.com>
2026-02-10 12:13:06 +0100

    Començar de 0

--- AppCrossfit/src/main/java/repository/CompeticionRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import com.example.appcrossfit.model.Competicion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompeticionRepository extends JpaRepository<Competicion, Long> {
-}
